c69de3c1244687cf8ee77e0f0f675558ec6c35e4
KMalif <khafidulmualif2019@gmail.com>
2024-02-15 09:36:17 +0700

    modify cart page

--- src/pages/Cart/index.jsx
+++ src/pages/Cart/index.jsx
@@ -23,54 +23,77 @@ const Cart = () => {
         </div>
 
         <div className={classes.cartContainer}>
-          <div className={`${classes.productContainer}`}>
-          <div className={classes.card}>
-          <div className={classes.imgCont}>
-            <img src={imgSrc} alt="img" />
-          </div>
-          <div className={classes.content}>
-            <div className={classes.productDetails}>
-              <div className={classes.titleCont}>
-                <div className={classes.title}>Expresso Ice coffee</div>
-                <div className={classes.desc}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, .</div>
+          <div className={classes.productContainer}>
+            <div className={classes.card}>
+              <div className={classes.imgCont}>
+                <img src={imgSrc} alt="img" />
               </div>
-              <div className={classes.price}>Rp 15000</div>
-            </div>
-            <div className={classes.actionCard}>
-              <div className={classes.btnCont}>
-                <button className={classes.btnPlus}>-</button>
-                <div className={classes.qty}>1</div>
-                <button className={classes.btnPlus} >+</button>
-              </div>
+              <div className={classes.content}>
+                <div className={classes.productDetails}>
+                  <div className={classes.titleCont}>
+                    <div className={classes.title}>Expresso Ice coffee</div>
+                    <div className={classes.desc}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, .</div>
+                  </div>
+                  <div className={classes.price}>Rp 15000</div>
+                </div>
+                <div className={classes.actionCard}>
+                  <div className={classes.btnCont}>
+                    <button className={classes.btnPlus}>-</button>
+                    <div className={classes.qty}>1</div>
+                    <button className={classes.btnPlus} >+</button>
+                  </div>
+                </div>
             </div>
             
+          </div>      
 
-          </div>
-          
-        </div>      
-          </div>
-          <div className={`${classes.totalContainer}`}>
-          <div className={classes.paySum}>
-          <div className={classes.total}>
-            <div className={classes.text}>
-              <p>Total</p>
-              Rp 150000
+          <div className={classes.productContainer}>
+            <div className={classes.card}>
+              <div className={classes.imgCont}>
+                <img src={imgSrc} alt="img" />
+              </div>
+              <div className={classes.content}>
+                <div className={classes.productDetails}>
+                  <div className={classes.titleCont}>
+                    <div className={classes.title}>Expresso Ice coffee</div>
+                    <div className={classes.desc}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, .</div>
+                  </div>
+                  <div className={classes.price}>Rp 15000</div>
+                </div>
+                <div className={classes.actionCard}>
+                  <div className={classes.btnCont}>
+                    <button className={classes.btnPlus}>-</button>
+                    <div className={classes.qty}>1</div>
+                    <button className={classes.btnPlus} >+</button>
+                  </div>
+                </div>
             </div>
-          </div>
-          <div className={classes.btnCont}>
+            
+          </div>  
+          </div>  
+        </div>
+
+        <div className={classes.totalContainer}>
+          <div className={classes.paySum}>
+            <div className={classes.total}>
+              <div className={classes.text}>
+                <p className={classes.totalTitle}>Total</p>
+                Rp 150000
+              </div>
+            </div>
+            <div className={classes.btnCont}>
             <button>
               Pesan
-              
             </button>
+            </div>
           </div>
         </div>
-          </div>
-        </div>
+      </div>
         
 
         
-      </div>     
-    </div>
+    </div>     
+  </div>
   );
 };
 
